f2610adcd1c886dfa2dbb6c2c45d9dd9c5bd12d3
Philipp Moritz <pcmoritz@gmail.com>
2016-02-29 18:08:01 -0800

    fix tensorflow preset to compile with latest master

--- tensorflow/cppbuild.sh
+++ tensorflow/cppbuild.sh
@@ -23,26 +23,16 @@ case $PLATFORM in
         ;;
 esac
 
-PROTOBUF_VERSION=master
-TENSORFLOW_VERSION=master
-
-download https://github.com/google/protobuf/archive/$PROTOBUF_VERSION.tar.gz protobuf-$PROTOBUF_VERSION.tar.gz
-download https://github.com/tensorflow/tensorflow/archive/$TENSORFLOW_VERSION.tar.gz tensorflow-$TENSORFLOW_VERSION.tar.gz
-
 mkdir -p $PLATFORM
 cd $PLATFORM
 
-echo "Decompressing archives"
-tar --totals -xzf ../protobuf-$PROTOBUF_VERSION.tar.gz
-tar --totals -xzf ../tensorflow-$TENSORFLOW_VERSION.tar.gz
+rm -rf tensorflow
+git clone --recurse-submodules https://github.com/tensorflow/tensorflow
 
 # Assumes Bazel is available in the path: http://bazel.io/docs/install.html
-cd tensorflow-$TENSORFLOW_VERSION/google
-rmdir protobuf || true
-ln -snf ../../protobuf-$PROTOBUF_VERSION protobuf
-cd ..
-patch -Np1 < ../../../tensorflow-$TENSORFLOW_VERSION.patch
+cd tensorflow
+patch -Np1 < ../../../tensorflow-master.patch
 ./configure
-bazel build -c opt //tensorflow/cc:libtensorflow.so $BUILDFLAGS
+bazel build -c opt --config=cuda //tensorflow/cc:libtensorflow.so $BUILDFLAGS
 
 cd ../..
